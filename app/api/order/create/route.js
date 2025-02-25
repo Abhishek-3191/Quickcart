7fcbac01bcfbe6d39185fa06a30a8c532f73d39b
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-02-25 15:51:07 +0530

    u

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -3,6 +3,7 @@ import connectDB from "@/config/db";
 import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { use } from "react";
 
 
 export async function POST(request) {
@@ -16,7 +17,23 @@ export async function POST(request) {
         const product=await Product.findById((item.product))
         return acc+product.offerPrice*item.quantity
        },0)
+       await inngest.send({
+        name:'order/created',
+        data:{
+            userId,
+            address,
+            items,
+            amount:amount+Math.floor(amount*.02),
+            data:Date.now()
+        }
+       })
+       //clear user cart
+       const user=await User.findById(userId)
+       user.cartItems={}
+       await user.save()
+       return NextResponse.json({success:true,message:'Order Placed'})
     } catch (error) {
+        console.error(error)
         return NextResponse.json({success:false,message:error.message})
     }
 }
